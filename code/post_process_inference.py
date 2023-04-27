53317c5cebaea34e8bb68f92b0d8132bc758bea7
Ubuntu <ubuntu@ip-172-31-12-165.ec2.internal>
2023-04-27 23:20:52 +0000

    code is updated!

--- code/post_process_inference.py
+++ code/post_process_inference.py
@@ -6,17 +6,31 @@ from scipy import stats
 from mt_metrics_eval import data
 
 wmt='wmt22'
-evs = data.EvalSet(wmt, 'zh-en')
+src_ref='ref'
+lang='zh-en'
+if wmt == 'wmt22' and src_ref == 'ref' and lang== 'zh-en':
+    suffix = ''
+else:
+    suffix = '_0_None'
+
+evs = data.EvalSet(wmt, lang)
 mqm_scores = evs.Scores('seg', 'mqm')
-sys_ls = set(mqm_scores) - evs.human_sys_names  #-{evs.std_ref}
-print(evs.std_ref)
-print(sys_ls)
-sescore3_ls, gt_ls = [], []
+sys_ls = set(mqm_scores) - evs.human_sys_names 
+
+new_sys_ls = []
 for sys in sys_ls:
-    ref_data = json.load(open(f'test_{wmt}_zh-en/ref/test_{wmt}_zh-en_{sys}_llama_ref_data.json'))
+    if mqm_scores[sys][0] != None:
+        new_sys_ls+=[sys]
+
+print(sys_ls)
+print(evs.std_ref)
+
+sescore3_ls, gt_ls = [], []
+for sys in new_sys_ls:
+    ref_data = json.load(open(f'test_{wmt}_{lang}/{src_ref}/test_{wmt}_{lang}_{sys}_llama_{src_ref}_data.json'))
     ref_ls = [ele['output'] for ele in ref_data['instances']]
     final_str = ''
-    lines = open(f'test_{wmt}_zh-en/SEScore3_output_222/test_{wmt}_zh-en_{sys}_llama_ref_data_0_None.txt', 'r').readlines()
+    lines = open(f'test_{wmt}_{lang}/sescore3_{src_ref}_output/test_{wmt}_{lang}_{sys}_llama_{src_ref}_data{suffix}.txt', 'r').readlines()
     for ele in lines:
         final_str += ele
 
@@ -27,8 +41,11 @@ for sys in sys_ls:
         score += (-1) * ele.count('Major/minor: Minor')
         score += (-5) * ele.count('Major/minor: Major')
         scores_ls += [score]
-    sescore3_ls += scores_ls
-    gt_ls += ref_ls
+
+    for sescore3, gt in zip(scores_ls, ref_ls):
+        # if gt < -5:
+        sescore3_ls += [sescore3]
+        gt_ls += [gt]
 
 res = stats.kendalltau(sescore3_ls, gt_ls)
 print(res)
